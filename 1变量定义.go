3f1e785bbb2543475dbfdec905d1af774a2255ff
yangyu0925 <172964776@qq.com>
2019-06-05 12:22:58 +0800

    定义变量

---
+++ 1变量定义.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+var (
+	aa = 33
+	ss = "kkk"
+)
+
+func variable()  {
+	var a int
+	var s string
+	fmt.Println(a, s)
+	fmt.Printf("%d %q \n", a, s)
+}
+
+func variableInit()  {
+	var a, b int = 3, 4
+	var s string = "abc"
+	fmt.Println(a, b, s)
+}
+
+func variableType()  {
+	var a, b, c, s = 3, 4, true, "hello"
+	fmt.Println(a, b, c, s)
+}
+
+func variableShort()  {
+	a, b, c, s := 3, 4, true, "hello world"
+	fmt.Println(a, b, c, s)
+}
+
+func main() {
+	fmt.Println("hello world")
+	variable()
+	variableInit()
+	variableType()
+	variableShort()
+	a, b, i, s1, s2 := true, false, 3, "hello", "world"
+	fmt.Println(a, b, i, s1, s2)
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # go
+
+##定义变量
+
+* 使用var关键字 
+    * var a, b, c bool
+    * var s1, s2 string = "hello", "world" 
+    * 可放在函数内，或直接放在包内
+    * 使用var (aa = 33)集中定义变量
+* 让编译器自动决定类型
+    * var a, b, i, s1, s2 = true, false, 3, "hello", "world"
+* 使用:=定义变量
+    * a, b, i, s1, s2 := true, false, 3, "hello", "world"  
+    * 只能在函数内使用
