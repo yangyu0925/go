222824f9e2f4a067c3acf94d0d0c0c55de82c98a
yangyu0925 <172964776@qq.com>
2019-06-09 10:39:16 +0800

    Map例题

---
+++ nonrepeating.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func lengthOfNonrepeatingSubstr(s string) int {
+	lastOccurred := make(map[byte]int)
+	start := 0
+	maxLength := 0
+
+	for i, ch := range []byte(s) {
+		if lastI, ok := lastOccurred[ch];
+			ok && lastI >= start {
+			start = lastOccurred[ch] + 1
+		}
+		if i - start + 1 > maxLength {
+			maxLength = i - start + 1
+		}
+		lastOccurred[ch] = i
+	}
+
+	return maxLength
+}
+
+func main() {
+	fmt.Println(lengthOfNonrepeatingSubstr("abcabcbb"))
+}
